5d95d14f949c977c087feace09d2911df9341dab
Joshua Nafziger <joshua@nafzigers.us>
2023-12-21 15:16:58 -0500

    Added !stats

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 #Imports
-import discord 
-import requests
-import os 
-import dotenv
-import json
-from discord.ext import tasks, commands
+import re, os, json, dotenv, requests, random, math
 from datetime import datetime
-import random
-import re
-
+import discord
+from discord.ext import tasks
 
 # Load environment variables
 dotenv.load_dotenv()
@@ -21,7 +15,7 @@ client = discord.Client(intents=Intents)
 custom_rooms = []
 
 maintenance_manager = [
-	493874564590338058, #Developer Joshua
+	493874564590338058,  # Developer Joshua
 ]
 
 last_anythin = datetime.now()
@@ -45,6 +39,70 @@ try:
 except json.decoder.JSONDecodeError:
 	custom_rooms = []
 
+def bedwarslvl(y):
+    level = 100 * int(y / 487000)
+    y = y % 487000
+    if y < 500:
+        return level + y / 500
+    level += 1
+    if y < 3500:
+        return level + (y - 500) / 1000
+    level += 1
+    if y < 3500:
+        return level + (y - 1500) / 2000
+    if y < 7000:
+        return level + (y - 3500) / 3500
+    level += 1
+    y -= 7000
+    return level + y / 5000
+
+async def addGameMode(message,color,userdata):
+	if "-Bedwars" in message.content:
+		embed = discord.Embed(title="Bedwars", color=color)
+		embed.set_thumbnail(url="https://hypixel.net/styles/hypixel-v2/images/game-icons/BedWars-64.png")
+		embed.add_field(name="Level", value=bedwarslvl(round(userdata["player"]["stats"]["Bedwars"]["Experience"]),2), inline=False) #Level
+		embed.add_field(name="Coins", value=bedwarslvl(userdata["player"]["stats"]["Bedwars"]["coins"]), inline=False) #Coins
+		embed.add_field(name="KDR", value=round(userdata["player"]["stats"]["Bedwars"]["kills_bedwars"]/userdata["player"]["stats"]["Bedwars"]["deaths_bedwars"],2), inline=False) #KDR
+		embed.add_field(name="FKDR", value=round(userdata["player"]["stats"]["Bedwars"]["final_kills_bedwars"]/userdata["player"]["stats"]["Bedwars"]["final_deaths_bedwars"],2), inline=False) #FKDR
+		await message.channel.send(embed=embed)
+	if "-SkyWars" in message.content:
+		#https://hypixel.net/styles/hypixel-v2/images/game-icons/Skywars-64.png
+		return
+	if "-Duels" in message.content:
+		#https://hypixel.net/styles/hypixel-v2/images/game-icons/Duels-64.png
+		return
+	if "-MurderMystery" in message.content:
+		#https://hypixel.net/styles/hypixel-v2/images/game-icons/MurderMystery-64.png
+		return
+	if "-TNTGames" in message.content:
+		#https://hypixel.net/styles/hypixel-v2/images/game-icons/TNT-64.png
+		return
+	
+
+async def getPlayerData(message):
+	try:
+		print(message.content[8:].find(" "))
+		path, uuid, fail = getHypixelData(message.content[7:message.content[8:].find(" ")+8])
+		if fail:
+			await message.reply("Please ask for a real player!")
+			return
+	except Exception as E123:
+		await message.reply("""Uses:\n!stats Name\n!stats Name -[Specific Game]\n\nGame Names:
+-Bedwars\n-SkyWars\n-Duels\n-MurderMystery\n-TNTGames""")
+		print(E123)
+		return 
+	try:
+		with open(path, 'r') as json_file:
+			userdata = json.load(json_file)
+	except Exception as err0r:
+		await message.channel.send(f"Failed! Error: {err0r}")
+		return
+	
+	if userdata["player"] is None:
+		await message.reply("Please ask for a real player!")
+		return
+	return userdata, uuid, path
+
 def getRank(userdata):
 	try:
 		try:
@@ -57,7 +115,6 @@ def getRank(userdata):
 				rank = userdata["player"]["rank"]
 			except:
 				try:
-					print("5")
 					PR = userdata["player"]["newPackageRank"]
 				except:
 					PR = userdata["player"]["packageRank"]
@@ -76,25 +133,30 @@ def getRank(userdata):
 	return rank
 
 
-async def getHypixelData(name):
+def getHypixelData(name):
 	uuid = requests.get(
 		url = f"https://api.mojang.com/users/profiles/minecraft/{name}"
 	).json()
+	try:
+		if uuid["errorMessage"] is not None:
+			return None, None, True
+	except:
+		Hypixel_data = requests.get(
+			url = "https://api.hypixel.net/v2/player",
+			params = {
+				"key": os.getenv('HYPIXEL'),
+				"uuid": uuid["id"]
+			}
+		).json()
+		
+		
+		name = uuid["id"]
 
-	Hypixel_data = requests.get(
-		url = "https://api.hypixel.net/v2/player",
-		params = {
-			"key": os.getenv('HYPIXEL'),
-			"uuid": uuid["id"]
-		}
-	).json()
-	name = uuid["id"]
+		with open(f"{hypixel_userdata_file_path}userdata/{name}.json", 'w') as json_file:
+			# Write the data to the JSON file
+			json.dump(Hypixel_data, json_file, indent=4)
 
-	with open(f"{hypixel_userdata_file_path}userdata/{name}.json", 'w') as json_file:
-		# Write the data to the JSON file
-		json.dump(Hypixel_data, json_file, indent=4)
-
-	return f"{hypixel_userdata_file_path}userdata/{name}.json", uuid["id"]
+		return f"{hypixel_userdata_file_path}userdata/{name}.json", uuid["id"], False
 
 async def create_Channel(message):
 	guild = client.get_guild(1175973620170895491)
@@ -156,7 +218,6 @@ async def on_ready():
 	
 @client.event
 async def on_voice_state_update(member, before, after):
-
 	try:
 		channel = client.get_channel(before.channel.id) #gets the channel you want to get the list from
 	except:
@@ -251,36 +312,15 @@ Starters, here is the ling: (https://aternos.org/server/)""")
 			
 		#Stats
 		if message.content.lower().startswith("!stats"):
-			try:
-				path, uuid = await getHypixelData(message.content[7:])
-			except:
-				await message.reply("""Uses:\n!stats Name\n!stats Name -[Specific Game]\n\nGame Names:
--Bedwars
--SkyWars
--Duels
--MurderMystery
--TNTGames""")
-				return
-			
-			try:
-				with open(path, 'r') as json_file:
-					userdata = json.load(json_file)
-			except:
-				await message.channel.send("Failed!")
-				return
-			
-			if userdata["player"] is None:
-				await message.reply("Please ask for a real player!")
-				return
-			
-			embed = discord.Embed(title=userdata["player"]["displayname"], color=random.randint(0, 0xFFFFFF))
+			userdata, uuid, path = await getPlayerData(message)
+			color = random.randint(0, 0xFFFFFF)
+			embed = discord.Embed(title=userdata["player"]["displayname"], color=color)
 			embed.set_thumbnail(url=f"https://mc-heads.net/avatar/{uuid}")
-
-
-
-			embed.add_field(name="Rank", value=getRank(userdata), inline=True)
+			embed.add_field(name="Rank", value=getRank(userdata), inline=False) #Rank
+			lvl = round((math.sqrt(userdata["player"]["networkExp"]+15312.5)-(125/math.sqrt(2)))/(25*math.sqrt(2)),2)
+			embed.add_field(name="Level", value=lvl, inline=False) #Level
 			await message.channel.send(embed=embed)
-
+			await addGameMode(message,color,userdata)
 			os.remove(path)
 
 	#Admin ONLY commands
@@ -288,12 +328,15 @@ Starters, here is the ling: (https://aternos.org/server/)""")
 		#Say Command
 		if message.content.lower().startswith("!say "):
 			await message.channel.send(message.content[5:])
+		#Go Under
 		elif message.content.lower().startswith("!gounder") and not testing_bot:
 			await message.channel.send("This bot is going into maintenance mode!")
 			maintenance = True
+		#Come Back
 		elif message.content.lower().startswith("!comeback") and not testing_bot:
 			await message.channel.send("This bot is back from maintenance mode!")
 			maintenance = False
+		#Veriable Sats
 		elif message.content.lower().startswith("!vstat"):
 			try:
 				await message.channel.send(f"{message.content[7:]}: {globals()[message.content[7:]]}")
